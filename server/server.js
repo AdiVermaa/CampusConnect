2ab0e2a1b8b8144410f1f5782c171389ec4fe85e
Aditya Verma <adityavermaa2006@gmail.com>
2025-11-18 12:40:09 +0530

    fixes

--- server/server.js
+++ server/server.js
@@ -9,7 +9,12 @@ import { db } from "./db.js";
 dotenv.config();
 
 const app = express();
-app.use(cors()); 
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+    credentials: true, // allow cookies to be sent
+  })
+);
 app.use(cookieParser());
 // Increase body size limit to handle base64 image uploads (50MB)
 app.use(express.json({ limit: "50mb" }));
